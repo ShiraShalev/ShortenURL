21653c1be06e1aa033d38451e807d8e9348b5bcb
Shira Shalev <shira@sodot.dev>
2025-02-24 10:50:46 +0200

    cr fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use axum::{
-    extract::Path,
-    response::{Redirect, Response},
-    routing::get,
-    Router,
-};
+use axum::{extract::Path, response::Redirect, routing::get, Router};
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::RwLock;
 use ShortenURL::generate_key;
+
+type State = Arc<RwLock<(HashMap<String, String>, HashMap<String, String>)>>;
 
 #[tokio::main]
 async fn main() {
@@ -31,7 +28,7 @@ async fn main() {
         )
         .fallback(fallback_handler);
 
-    let listener = match tokio::net::TcpListener::bind("0.0.0.0:3000").await {
+    let listener = match tokio::net::TcpListener::bind("0.0.0.0:3001").await {
         Ok(listener) => listener,
         Err(e) => {
             eprintln!("Failed to bind to port 3000: {}", e);
@@ -48,32 +45,37 @@ async fn fallback_handler() -> Redirect {
     Redirect::temporary("/404")
 }
 
-async fn shorten_url(
-    Path(url): Path<String>,
-    state: Arc<RwLock<(HashMap<String, String>, HashMap<String, String>)>>,
-) -> Result<String, Response> {
+async fn shorten_url(Path(path): Path<String>, state: State) -> String {
     let (url_to_key, key_to_url) = &mut *state.write().await;
     let uri = url_to_key
-        .entry(url.clone())
-        .or_insert_with(|| generate_key());
-    key_to_url.entry(uri.clone()).or_insert_with(|| url.clone());
+        .entry(path.clone())
+        .or_insert_with(|| generate_unique_key(key_to_url));
+    key_to_url.entry(uri.clone()).or_insert_with(|| path);
 
-    Ok(format!(
-        "Your new URL: http://localhost:3000/access/{}",
-        uri
-    ))
+    format!("Your new URL: http://localhost:3000/access/{}", uri)
 }
 
-async fn redirect_url(
-    Path(url): Path<String>,
-    state: Arc<RwLock<(HashMap<String, String>, HashMap<String, String>)>>,
-) -> Result<Redirect, Redirect> {
+fn generate_unique_key(key_to_url: &HashMap<String, String>) -> String {
+    loop {
+        let key = generate_key();
+        if !key_to_url.contains_key(&key) {
+            return key;
+        }
+    }
+}
+
+async fn redirect_url(Path(path): Path<String>, state: State) -> Result<Redirect, Redirect> {
     let (_, ref key_to_url) = *state.read().await;
-    match key_to_url.get(&url) {
+    match key_to_url.get(&path) {
         None => Err(fallback_handler().await),
         Some(long_url) => {
-            println!("Redirecting from {} to {}", url, long_url);
-            let go_to = format!("http://{}", long_url);
+            println!("Redirecting from {} to {}", path, long_url);
+            let go_to = if !long_url.starts_with("http://") && !long_url.starts_with("https://") {
+                format!("http://{}", long_url)
+            } else {
+                long_url.clone()
+            };
+            print!("Redirecting to {}", go_to);
             Ok(Redirect::permanent(&go_to))
         }
     }
